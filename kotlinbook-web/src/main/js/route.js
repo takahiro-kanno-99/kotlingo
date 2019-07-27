81608276a0f031f6f354cfa34b247935b65d3fd0
takahiro.kanno <ooparts99@icloud.com>
2019-07-27 15:06:38 +0900

    つけるクリックでアラート表示

--- kotlinbook-web/src/main/js/route.js
+++
@@ -1,8 +0,0 @@
-import confirm from "./pages/confirm.vue";
-
-export default[
-    {
-        path:'confirm',
-        component:confirm
-    }
-]
---
+++ kotlinbook-web/src/main/resources/js/top.js
@@ -0,0 +1,10 @@
+var myVueApp = new Vue({
+    el:"#list-group-item"
+    ,methods:{
+        tsukeru:function(){
+            alert('家計簿をつけるのかい？')
+        }
+        
+    }
+
+})
